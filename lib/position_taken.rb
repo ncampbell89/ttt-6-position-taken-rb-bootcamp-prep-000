950e64895a7380e8666b909309300ea58b83771f
Nicole Campbell <nscampbell303@gmail.com>
2017-11-19 16:47:23 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,15 +1,9 @@
 def position_taken?(board, index)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
-  board[index]
 
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   elsif board[index] == "X" || board[index] == "O"
     return true
   end
+  
 end
